a5d81ae83b7b8d1c08816c7730bc4a7f32f3dc74
Aron Winston <aronwinston2792@gmail.com>
2018-11-29 15:47:00 -0500

    added event listener to java script

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+'use strict'
+
+alert("Hello and Welcome to Munder Difflin.  Company for Legends only")
+
+let sections = document.getElementsByTagName('section');
+let section1 = section[0];
+let section2 = section[1];
+let section3 = section[2];
+
+window.addEventListener ('scroll',(event) => {
+    if (window.scrollY > 740px){
+        section1.classList.add('fadein');
+    }
+
+    if (window.scrollY > 1240px) {
+        section2.classList.add('fadein');
+    }
+
+    if (window.scrollY > 1740px){
+        section3.classList.add('fadein')
+    }
+
+});
+
+
+
+
+
+
 
