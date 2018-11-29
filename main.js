9e57d4b192bed810d04aca035c91956e9577d1e4
Aron Winston <aronwinston2792@gmail.com>
2018-11-29 16:49:31 -0500

    completed scroll animation to munder difflin

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 'use strict'
 
-alert("Hello and Welcome to Munder Difflin.  Company for Legends only")
+// alert("Hello and Welcome to Munder Difflin.  Company for Legends only")
 
 let sections = document.getElementsByTagName('section');
-let section1 = section[0];
-let section2 = section[1];
-let section3 = section[2];
+let section1 = sections[0];
+let section2 = sections[1];
+let section3 = sections[2];
 
-window.addEventListener ('scroll',(event) => {
-    if (window.scrollY > 740px){
-        section1.classList.add('fadein');
+window.addEventListener ('scroll', (event) => {
+    if (window.scrollY > 740){
+        section1.classList.add("fadein");
     }
 
-    if (window.scrollY > 1240px) {
-        section2.classList.add('fadein');
+    if (window.scrollY > 1240) {
+        section2.classList.add("fadein");
     }
 
-    if (window.scrollY > 1740px){
-        section3.classList.add('fadein')
+    if (window.scrollY > 1740) {
+        section3.classList.add("fadein")
     }
-
-});
+})
 
 
 
